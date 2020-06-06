dd9bb3bd4c7835c31a41eb73ea0c5aac0845ac1f
sumins2 <99tnals@naver.com>
2020-06-06 18:23:05 +0900

    base만듦

--- nasosunpj/app/views.py
+++ nasosunpj/app/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render,redirect
 from .models import Product, Result, Offer, Requirement, User, Review
 from .crawl import GetProductInfo
 # Create your views here.
+def main(request):
+    return render(request, 'main.html')
