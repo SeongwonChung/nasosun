9daf7ea15835b454753ebf99b4059b4012c0fc95
Seongwon Chung <syc1013@korea.ac.kr>
2020-06-06 13:12:04 +0900

    initial commit

---
+++ nasosunpj/app/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length = 200)
+    price = models.IntegerField()
+    product_url = models.TextField()
+    img_url = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
+class Offer(models.Model):
+    user_no = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "offers")
+
+
+class Result(models.Model):
+    offer_no = models.ForeignKey(Offer, on_delete = models.CASCADE, related_name = "results")
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return str(self.pk) + "/"+ str(self.offer_no)
+
+class Requirement(models.Model):
+    GENDER = (
+        ('남자','남자'),
+        ('여자','여자')
+    )
+
+    TASTE = (
+        ('실용','실용적'),
+        ('고급','고급스러운')
+    )
+
+    TEMPERAMENT = (
+        ('외향적','외향적'),
+        ('내향적','내향적')
+    )
+    offer_no = models.OneToOneField(Offer, on_delete = models.CASCADE, primary_key = True)
+    gender = models.CharField(max_length = 2, choices = GENDER)
+    age = models.IntegerField()
+    taste = models.CharField(max_length = 2, choices = TASTE) #취향
+    temperament = models.CharField(max_length = 3, choices = TEMPERAMENT) #성향
+    purpose = models.CharField(max_length = 100)
+    budget = models.IntegerField()
+
+    def __str__(self):
+            return str(self.pk) + "/"+ str(self.offer_no)
+
+class Review(models.Model):
+    result_no = models.OneToOneField(Result, on_delete = models.CASCADE, primary_key = True)
+    user_no = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "reviews")
+    product_no = models.ForeignKey(Product, on_delete = models.CASCADE, related_name = "products")
+    satisfy = models.IntegerField()
+
+    def __str__(self):
+        return self.result_no
---
+++ nasosunpj/app/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render,redirect
+from .models import Product, Result, Offer, Requirement, User, Review
+from .crawl import GetProductInfo
+# Create your views here.
