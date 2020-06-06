2824d83a793c09cbf978bcba01386262cb767366
Seongwon Chung <syc1013@korea.ac.kr>
2020-06-06 22:33:08 +0900

    for_you 수정중

--- nasosunpj/app/views.py
+++ nasosunpj/app/views.py
@@ -7,22 +7,34 @@ from django.contrib import auth
 
 def for_you(request):
     if request.method == "POST":
-         new_offer = Offer.objects.create(
-             user_no = request.user.pk
-         )
-         new_reslut = Result.objects.create(
-             offer_no = new_offer.pk
-         )
-         new_requirement = Requirement.objects.create(
-             offer_no = new_offer.pk,
-             gender =request.POST['gender'],
-             age =request.POST['age'],
-             taste =request.POST['taste'],
-             temperament =request.POST['temperament'],
-             purpose =request.POST['purpose'],
-             budget =request.POST['budget'],
-         )
-         return  redirect('spinner', new_result.offer_no)
+        print(request.POST['taste'])
+        new_offer = Offer.objects.create(
+            user_no = request.user
+        )
+        new_reslut = Result.objects.create(
+            offer_no = new_offer
+        )
+
+        temper_score = request.POST['temperament']
+        print(temper_score, type(temper_score))
+        if int(temper_score) > 50:
+            temp = "감성"
+        else:
+            temp = "실용"
+            
+        taste = request.POST['taste'][0]
+        
+
+        new_requirement = Requirement.objects.create(
+            offer_no = new_offer,
+            gender =request.POST['gender'],
+            age =request.POST['age'],
+            taste = taste, 
+            temperament =temp,
+            purpose =request.POST['purpose'],
+            budget =request.POST['budget'],
+        )
+        return  redirect('spinner', new_result.offer_no)
     return render(request, 'for_you.html')
 
 def signup(request):
