7b7312c896e2e366174bda25bba53a1ab4924f1d
seeke <seekeryang7@gmail.com>
2020-06-06 18:38:51 +0900

    for_you

--- nasosunpj/app/views.py
+++ nasosunpj/app/views.py
@@ -2,3 +2,28 @@ from django.shortcuts import render,redirect
 from .models import Product, Result, Offer, Requirement, User, Review
 from .crawl import GetProductInfo
 # Create your views here.
+
+
+def for_you(request):
+    if request.method == "POST":
+         new_offer = Offer.objects.create(
+             user_no = request.user.pk
+         )
+         new_reslut = Result.objects.create(
+             offer_no = new_offer.pk
+         )
+         new_requirement = Requirement.objects.create(
+             offer_no = new_offer.pk,
+             gender =request.POST['gender'],
+             age =request.POST['age'],
+             taste =request.POST['taste'],
+             temperament =request.POST['temperament'],
+             purpose =request.POST['purpose'],
+             budget =request.POST['budget'],
+         )
+         return  redirect('spinner', new_result.offer_no)
+    return render(request, 'for_you.html')
+
+def spinner(request, request_pk):
+    request = request.objects.get(pk=request_pk)
+    return render(request, 'spinner.html')
